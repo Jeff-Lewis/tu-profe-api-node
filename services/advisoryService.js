dc4568e86692390861d9ec3fcd1fefee81da275a
daprieto1 <da.prieto1@uniandes.edu.co>
2017-04-25 23:27:36 -0500

    create advisory service

--- services/advisoryService.js
+++ services/advisoryService.js
@@ -1,7 +1,21 @@
 var uuidV4 = require('uuid/v4');
 var Promise = require('promise');
 
+var AdvisoryService = require('../models/advisoryService');
 var AdvisoryServiceServices = {};
+
+AdvisoryServiceServices.createAdvisoryService = advisoryService => {
+    return AdvisoryServiceServices.calculate(advisoryService)
+        .then(advisoryService => {
+            return new Promise((resolve, reject) => {
+                advisoryService.id = uuidV4();
+                AdvisoryService.create(advisoryService, function (err, newAdvisoryService) {
+                    if (err) reject(err);
+                    else resolve(newAdvisoryService);
+                });
+            });
+        });
+};
 
 AdvisoryServiceServices.calculate = advisoryService => {
     return new Promise((resolve, reject) => {
