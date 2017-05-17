b04a67c9709ab01802abb9b670ad8791ccb138d2
daprieto1 <da.prieto1@uniandes.edu.co>
2017-05-17 21:17:33 +0000

    first calculate service cost

--- services/advisoryService.js
+++ services/advisoryService.js
@@ -5,6 +5,7 @@ var AdvisoryService = require('../models/advisoryService');
 var AdvisoryServiceType = require('../models/enum/advisoryServiceType');
 
 var NotificationServices = require('../services/notification');
+var UtilsServices = require('../services/utils');
 var AdvisoryServiceServices = {};
 
 /**
@@ -41,11 +42,91 @@ AdvisoryServiceServices.createAdvisoryService = advisoryService => {
  */
 AdvisoryServiceServices.calculate = advisoryService => {
     return new Promise((resolve, reject) => {
-        var cost = 0;
-        advisoryService.cost = {
-            baseCost: cost
+        
+        var conf = {
+            F:1-0.0299*1.19,
+            G:1.06,
+            E:600*1.19
         };
-        resolve(advisoryService);
+        
+        var factor = (B,C,X)=>{
+            console.log(B,C,X);
+            return B-(C*(X-8)/2);
+        };
+        
+        var epayCo = (V,X) => {
+            return (V*X + conf.E)/conf.F*conf.G;
+        };
+        
+        var specificSmallerThan8 = (A,X) => {
+            return A*X;    
+        };
+        
+        var specificOneStudent = (X,B,C,D) => {
+            var V = factor(B,C,X)/D;
+            console.log(V);
+            return epayCo(V,X);
+        };
+        
+        var specificMoreThanTwoStudents = (X,B,C,D,L,M) => {
+            var V = ((L*X) + M - factor(B,C,X))/D;
+            return epayCo(V,X);
+        };
+        
+        var tutor = (L,X,M,N,O,P,D) => {
+            var V = (L*X + M + (N*X + O)*(P - 1))/D; 
+            return epayCo(V,X);
+        };
+        
+        var total = 0;
+        
+        switch (advisoryService.type) {
+            case 1:
+                conf.L=-127.2;
+                conf.M=4997.4;
+                conf.N=-416.93;
+                conf.O=5639.2;
+                conf.D=0.8;
+                var x = advisoryService.sessionsPerWeek * 2 * 4;
+                total = tutor(conf.L,x,conf.M,conf.N,conf.O,advisoryService.numStudents,conf.D);
+                break;
+            case 2:
+                var costFunction = specificMoreThanTwoStudents;
+                var X = advisoryService.timePerSession * advisoryService.numSessions;
+                switch (advisoryService.numStudents) {
+                    case 1:
+                        conf.A = 17490;
+                        conf.B = 16366.2530739996;	
+                        conf.C = 209.823757358969;	
+                        conf.D = 0.7
+                        costFunction = specificOneStudent;
+                        break;
+                    case 2:
+                        conf.A = 25440;
+                        break;
+                    case 3:
+                        conf.A = 33390;
+                        break;
+                    case 4:
+                        conf.A = 41340;
+                        break;
+                    default:
+                        reject('No es posible calcular un valor para este número de estudiantes.');
+                        break;
+                }
+                if(X < 8){
+                    total = specificSmallerThan8(conf.A, X);
+                }else{
+                    total = costFunction(X,conf.B,conf.C,conf.D,conf.L,conf.M);
+                }
+                break;
+            default:
+            reject('No es posible calcular el valor a este tipo de servicio.');
+                break;
+        }
+        advisoryService.total=UtilsServices.ceil10(total,2);
+        return resolve(advisoryService);
+        
     });
 };
 
