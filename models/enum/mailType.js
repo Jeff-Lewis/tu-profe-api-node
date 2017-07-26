e4b4292a617735b64f09f892b20835333e68f565
daprieto1 <da.prieto1@uniandes.edu.co>
2017-07-26 21:42:12 +0000

    add more mail types

--- models/enum/mailType.js
+++ models/enum/mailType.js
@@ -3,7 +3,9 @@ var Enum = require('enum');
 var MailType = new Enum({
     'STUDENT_SIGN_UP': 1,
     'TEACHER_SIGN_UP': 2,
-    'FORGOT_PASSWORD': 3
+    'FORGOT_PASSWORD': 3,
+    'RESTORED_PASSWORD': 4,
+    'SERVICE_CREATED': 5
 });
 
 module.exports = MailType;
