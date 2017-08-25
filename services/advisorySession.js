7c740f5126edac0a636927620c29e73b8376c98d
daprieto1 <da.prieto1@uniandes.edu.co>
2017-08-25 09:54:04 -0500

    delete advisory from teacher when finished

--- services/advisorySession.js
+++ services/advisorySession.js
@@ -1,5 +1,9 @@
 var SessionState = require('../models/enum/sessionState');
 
+var AdvisoryServiceState = require('../models/enum/advisoryServiceState');
+
+var LogService = require("../services/log")();
+var TeacherService = require('../services/teacher');
 var AdvisoryServiceServices = require('../services/advisoryService');
 
 var AdvisorySessionServices = {};
@@ -15,13 +19,27 @@ AdvisorySessionServices.updateAdvisorySession = (advisoryServiceId, sessionId, s
             } else if (sessionUpdated.state === SessionState.IN_PROCESS.value) {
                 session.inProgressTime = new Date();
                 session.teacher = advisoryService.teacher;
+                LogService.log('AdvisorySession', 'updateAdvisorySession', `Advisory Service ${advisoryServiceId} Session ${session.id} IN_PROCESS`, 'info', {});
             } else if (sessionUpdated.state === SessionState.FINISHED.value) {
                 session.finishedTime = new Date();
-                session.realDuration = session.finishedTime - new Date(session.inProgressTime);                
+                session.realDuration = session.finishedTime - new Date(session.inProgressTime);
+                LogService.log('AdvisorySession', 'updateAdvisorySession', `Advisory Service ${advisoryServiceId} Session ${session.id} FINISHED`, 'info', {});
             }
 
             session.state = sessionUpdated.state;
             advisoryService.sessions[index] = session;
+            
+            if (advisoryService.sessions.every(session => session.state === SessionState.FINISHED.value)) {
+                advisoryService.state = AdvisoryServiceState.FINISHED.value;
+                TeacherService.getTeacherById(advisoryService.teacher)
+                    .then(teacher => {                        
+                        teacher.advisoryServices = teacher.advisoryServices.filter(advisory => advisory !== advisoryServiceId);
+                        TeacherService.updateTeacher(teacher.id, teacher);
+                    })
+                    .catch(err => LogService.log('AdvisorySession', 'updateAdvisorySession', 'error', 'err', err));
+                LogService.log('AdvisorySession', 'updateAdvisorySession', `Advisory Service ${advisoryServiceId} FINISHED`, 'info', {});
+            }
+
             return AdvisoryServiceServices.updateAdvisoryService(advisoryService.id, advisoryService);
         });
 };
